6f50054e1e49e455a2e93ef1876151e28cb24a51
mansirathor27 <mansirathor575@gmail.com>
2025-11-15 16:22:00 +0530

    setup: initial project with tailwind css configuration

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+        <h1 className='text-6xl bg-blue-500 '>
+          Chat Application
+        </h1>
+    </div>
+  )
+}
+
+export default App
