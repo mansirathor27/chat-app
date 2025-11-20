9ae4a56801b42b8a9fac3f99b70ff81215beb663
mansirathor27 <mansirathor575@gmail.com>
2025-11-20 16:18:28 +0530

    feat: add messaging API routes with Cloudinary image support

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@
 // middleware to protect the routes
 
 import jwt from "jsonwebtoken";
-import User from "../models/User";
+import User from "../models/User.js";
 
 export const protectRoute = async(req, res, next) =>{
     try {
--- server/server.js
+++ server/server.js
@@ -4,10 +4,36 @@ import cors from "cors"
 import http from "http";
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
-
+import messageRouter from './routes/messageRoutes.js';
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app)
+
+// initialise socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+// store online users
+export const userSocketMap = {}; //{userId: socketId}
+
+// socket.io connection handler
+io.on("Connection", (socket) =>{
+    const userId = socket.handshake.query.userId;
+    console.log("User connected", userId);
+    if(userId) userSocketMap[userId] = socket.id;
+
+    // emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () =>{
+        console.log("User disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+    })
+})
+
 
 // middleware setup
 app.use(express.json({limit: "4mb"}));
@@ -17,6 +43,7 @@ app.use(cors());
 // routes setup
 app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter)
 await connectDB();
 
 const PORT = process.env.PORT || 5000;
